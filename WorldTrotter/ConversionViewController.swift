e90be85b41e01bf86825b452bd0730a7182a2ab4
floodliu <floodliu@gmail.com>
2016-02-18 16:19:21 +0800

    add new features into WorldTrotter: read input from text field, convert fahrenheit to celsius, update celsius label, hide input method after input completed.

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by hongtao on 16/2/18.
+//  Copyright © 2016年 floodliu. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5 / 9)
+        } else {
+            return nil
+        }
+    }
+
+    let numberFormatter: NSNumberFormatter = {
+        let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+
+    @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
+        if let text = textField.text, value = Double(text) {
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
+    }
+}
