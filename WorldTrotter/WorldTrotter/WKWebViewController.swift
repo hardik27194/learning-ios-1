ca1af5ed14c567bbf584ae2fdbe686d80a0d617d
floodliu <floodliu@gmail.com>
2016-03-14 05:13:57 +0800

    add WebIcon into TabBar and change WKWebView's position and size.

--- WorldTrotter/WorldTrotter/WKWebViewController.swift
+++ WorldTrotter/WorldTrotter/WKWebViewController.swift
@@ -13,11 +13,21 @@ class WKWebViewController: UIViewController {
     var webView: WKWebView!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         webView = WKWebView(frame: view.bounds)
         view.addSubview(webView)
         
         let url = NSURL(string: "http://www.jianshu.com")
         let request = NSURLRequest(URL: url!)
         webView.loadRequest(request)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let yOffset = topLayoutGuide.length * 2
+        webView.bounds.origin.y += yOffset
+        webView.bounds.size.height -= yOffset
     }
 }
