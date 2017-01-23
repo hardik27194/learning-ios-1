a455ba34d94f05b24efb358b8bfe35a080a03835
floodliu <floodliu@gmail.com>
2017-01-23 12:01:21 +0800

    add logs.

--- LittleHamster/LittleHamster/ViewController.m
+++ LittleHamster/LittleHamster/ViewController.m
@@ -35,8 +35,10 @@
     [super viewDidAppear:animated];
     
     if ([HKHealthStore isHealthDataAvailable]) {
+        NSLog(@"当前设备支持HealthKit");
         self.healthStore = [[HKHealthStore alloc] init];
     } else {
+        NSLog(@"当前设备不支持HealthKit");
         self.healthStore = nil;
         NSString *title = @"提醒";
         NSString *message = @"您的设备不支持HealthKit，无法使用此应用。请按“确定”来退出。";
@@ -67,8 +69,10 @@
 
     [self.healthStore requestAuthorizationToShareTypes:shareTypes readTypes:readTypes completion:^(BOOL success, NSError * _Nullable error) {
         if (success) {
+            NSLog(@"已获得HealthKit的读写授权");
             [self readStepsCount:YES];
         } else {
+            NSLog(@"未获得HealthKit的读写权限");
             NSString *title = @"提醒";
             NSString *message = @"未能获得HealthKit权限，无法使用此应用。请按“确定”来退出。";
             [self showAlertViewWithTitle:title message:message];
@@ -85,8 +89,11 @@
     NSPredicate *predicate = [self createPredicate];
     HKStatisticsQuery *statisticsQuery = [[HKStatisticsQuery alloc] initWithQuantityType:self.stepsCountType quantitySamplePredicate:predicate options:HKStatisticsOptionCumulativeSum completionHandler:^(HKStatisticsQuery * _Nonnull query, HKStatistics * _Nullable result, NSError * _Nullable error) {
         if (result) {
+            NSLog(@"读取步数成功");
             double totalSteps = [result.sumQuantity doubleValueForUnit:[HKUnit countUnit]];
             self.stepsCountLabel.text = [NSString stringWithFormat:@"%d", (int)totalSteps];
+        } else {
+            NSLog(@"读取步数失败");
         }
         
         [self showWaitIndicatorAndLabel:NO];
@@ -97,19 +104,21 @@
 }
 
 - (void)enableUserInput:(BOOL)enabled {
+    NSLog(@"%@用户输入", enabled ? @"允许" : @"禁止");
     [self.stepsCountInputter setEnabled:enabled];
     [self.writterButton setEnabled:enabled];
 }
 
 - (void)showWaitIndicatorAndLabel:(BOOL)show {
     if (show) {
+        NSLog(@"显示wait indicator");
         [self.waitLabel setHidden:NO];
         
-        if ([self.waitIndicator isAnimating]) {
-            [self.waitIndicator stopAnimating];
+        if (![self.waitIndicator isAnimating]) {
+            [self.waitIndicator startAnimating];
         }
-        [self.waitIndicator startAnimating];
     } else {
+        NSLog(@"隐藏wait indicator");
         [self.waitLabel setHidden:YES];
         
         if ([self.waitIndicator isAnimating]) {
@@ -144,8 +153,10 @@
     HKQuantitySample *stepsQuantitySampel = [HKQuantitySample quantitySampleWithType:self.stepsCountType quantity:stepsQuantity startDate:[NSDate date] endDate:[NSDate date]];
     [self.healthStore saveObject:stepsQuantitySampel withCompletion:^(BOOL success, NSError * _Nullable error) {
         if (success) {
+            NSLog(@"写入步数成功");
             [self readStepsCount:NO];
         } else {
+            NSLog(@"写入数据失败");
             [self showWaitIndicatorAndLabel:NO];
             [self enableUserInput:YES];
         }
